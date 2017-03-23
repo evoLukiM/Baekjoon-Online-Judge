8d3ef73f76bb72383a4dc7e495e17a285a42e524
Sungho_Lee <thyme38@naver.com>
2017-03-23 14:18:49 +0900

    2017.03.23

--- code.cpp
+++ code.cpp
@@ -495,3 +495,180 @@ int main()
 		printf("\n");
 	}
 }
+
+//-----------------------------------------
+
+//2017.03.20
+//2441
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int n = 0;
+	scanf("%d", &n);
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int k = 0; k < i; k++)
+		{
+			printf(" ");
+		}
+		for (int j = 0; j < n-i; j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+}
+
+//-----------------------------------------
+
+//2017.03.20
+//2442
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int n = 0;
+	scanf("%d", &n);
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int k = 0; k < n-(i+1); k++)
+		{
+			printf(" ");
+		}
+		for (int j = 0; j < (2*(i + 1) - 1); j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+}
+
+//-----------------------------------------
+
+//2017.03.20
+//2444
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int n = 0;
+	scanf("%d", &n);
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int k = 0; k < n-(i+1); k++)
+		{
+			printf(" ");
+		}
+		for (int j = 0; j < (2*(i + 1) - 1); j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+
+	for (int i = 0; i < n - 1; i++)
+	{
+		for (int k = 0; k < i+1; k++)
+		{
+			printf(" ");
+		}
+		for (int j = 0; j < (2 * (n - 1) - 1) - 2 * i; j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+}
+
+//-----------------------------------------
+
+//2017.03.20
+//2445
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int n = 0;
+	scanf("%d", &n);
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int k = 0; k < i + 1; k++)
+		{
+			printf("*");
+		}
+		for (int j = 0; j < (n * 2) - 2 * (i + 1); j++)
+		{
+			printf(" ");
+		}
+		for (int k = 0; k < i + 1; k++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+	//--------------
+	for (int i = 0; i < n - 1; i++)
+	{
+		for (int k = 0; k < (n-1)-i; k++)
+		{
+			printf("*");
+		}
+		for (int j = 0; j < 2*(i+1); j++)
+		{
+			printf(" ");
+		}
+		for (int k = 0; k < (n - 1) - i; k++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+}
+
+//-----------------------------------------
+
+//2017.03.20
+//2446
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int n = 0;
+	scanf("%d", &n);
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < i; j++)
+		{
+			printf(" ");
+		}
+		for (int k = 0; k < 2*n-(i+1)-i; k++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+	//--------------
+	for (int i = 0; i < n - 1; i++)
+	{
+		for (int k = 0; k < (n-2)-i; k++)
+		{
+			printf(" ");
+		}
+		for (int j = 0; j < 2*(i+2)-1; j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+}
